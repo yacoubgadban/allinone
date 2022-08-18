f1a7b41360819db172d40a65e9d83cc1f0045f7e
yacoubgadban <yacoub.gadban@gmail.com>
2022-08-18 08:57:58 +0300

    reeducer

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,53 @@
+import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+const reducer=(state , action)=>{
+  switch (action.type) {
+    
+    case "FETCH_REQUEST":  
+      return {...state , loading:true};
+    case "FETCH_SUCCESS":  
+      return {...state ,products:action.payload , loading:false};
+    case "FETCH_FAIL":  
+      return {...state ,error:action.payload , loading:false};
+    default:
+      return state;
+  }
+}
+
 function HomeScreen(){
+
+  const [{products , loading ,error},dispatch]=useReducer(logger(reducer),{products:[], loading:true , error:''})
+    useEffect(() => {
+      const fetchData = async () => {
+            dispatch({type:'FETCH_REQUEST'})
+            try{
+              const result = await axios.get('/api/products');
+              dispatch({type:'FETCH_SUCCESS',payload:result.data})
+            }catch(e){
+              dispatch({type:'FETCH_FAIL' ,payload:error.message})
+            } 
+      };
+      fetchData();
+    }, []);
+
     return (
     <div>
          <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
+        <div className="products">        
+          
+          {
+            //loading
+            loading?
+              <div>loading...</div>
+              :
+             //error 
+            error?
+              <div>{error}</div>
+              :
+              //success get products
+            products.map((product) => (
             <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
@@ -21,6 +63,7 @@ function HomeScreen(){
               </div>
             </div>
           ))}
+         
         </div>
     </div>
     )
